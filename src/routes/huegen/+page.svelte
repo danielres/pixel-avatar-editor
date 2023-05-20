186202eb6280ebabddd49fbc2763145b40eb6bea
Daniel Reszka <dannn.r@gmail.com>
2023-05-20 22:59:32 +0200

    pages: add huegen (attempt 2)

---
+++ src/routes/huegen/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import Selectors from './Selectors.svelte'
+
+  let lums: number[]
+  let hues: number[]
+</script>
+
+<Selectors bind:lums bind:hues />
+
+<div>
+  <pre>{hues}</pre>
+  <pre>{lums}</pre>
+</div>
