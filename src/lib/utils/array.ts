9fa423cd0ec9dfeb51ac8ebe8c9673a6d29ca93e
Daniel Reszka <dannn.r@gmail.com>
2023-05-21 00:43:00 +0200

    utils/array: add grouper()

--- src/lib/utils/array.ts
+++ src/lib/utils/array.ts
@@ -17,3 +17,26 @@ export function onlyUnique<T>(value: T, index: number, array: T[]): boolean {
 export function onlyUniqueTuples<T>(value: T, index: number, array: T[]): boolean {
   return array.map((val) => JSON.stringify(val)).indexOf(JSON.stringify(value)) === index
 }
+
+/**
+ * Creates a collection of arrays/tuples from a single array.
+ * source: https://stackoverflow.com/a/16758242/564632
+ *
+ * @param  array Original array
+ * @param  groupSize Size of each group/tuple
+ *
+ * @example
+ *  const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, ...]
+ *  const result = grouper(array, 2)
+ *  // => [[1, 2], [3, 4], [5, 6], [7, 8], [9, 10], ...]
+ */
+export function grouper<A>(array: A[], groupSize: number): A[][] {
+  const result = []
+  let i = 0
+  const n = array.length
+  while (i < n) {
+    result.push(array.slice(i, i + groupSize))
+    i += groupSize
+  }
+  return result
+}
