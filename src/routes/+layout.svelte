57fd8b9c3b677281315bdc81a2987f31a0ec5150
Daniel Reszka <dannn.r@gmail.com>
2023-05-20 22:59:12 +0200

    pages: add "class" (attempt 01) + main layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+  import type { LayoutData } from './$types'
+
+  export let data: LayoutData
+</script>
+
+<div class="shell">
+  <nav>
+    <ul>
+      <li><a href="/class">Class & canvas</a></li>
+      <li><a href="/huegen">Huegen</a></li>
+      <li><a href="/grid" data-sveltekit-reload>Grid</a></li>
+    </ul>
+  </nav>
+
+  <slot />
+</div>
+
+<style>
+  :global(*) {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+    font-family: sans-serif;
+  }
+  ul {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+    display: flex;
+    gap: 1rem;
+  }
+
+  .shell {
+    display: grid;
+    gap: 1rem;
+  }
+</style>
