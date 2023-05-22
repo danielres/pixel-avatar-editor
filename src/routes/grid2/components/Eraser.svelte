49600c39c048200cbc36ff5b7be30ee3dee8a2e6
Daniel Reszka <dannn.r@gmail.com>
2023-05-22 23:05:12 +0200

    Icon: extract eraser

--- src/routes/grid2/components/Eraser.svelte
+++ src/routes/grid2/components/Eraser.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import Icon from './Icon.svelte'
   import type { StateStore } from '../stores'
 
   export let stateStore: StateStore
@@ -13,16 +14,7 @@
   title="Erase"
   class:isActive={!!$stateStore.isUsingEraser}
 >
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    fill="none"
-    viewBox="0 0 24 24"
-    stroke-width="1.5"
-    stroke="currentColor"
-    class="w-6 h-6"
-  >
-    <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
-  </svg>
+  <Icon kind="eraser" />
 </button>
 
 <style>
