a2e3dfb4ef260ced34459dba19b18a57ba7d39e0
Daniel Reszka <dannn.r@gmail.com>
2023-05-22 21:50:44 +0200

    add previews

--- src/routes/grid2/+page.svelte
+++ src/routes/grid2/+page.svelte
@@ -2,11 +2,12 @@
   import DrawingBoard from './components/DrawingBoard.svelte'
   import Eraser from './components/Eraser.svelte'
   import Palette from './components/Palette.svelte'
+  import Preview from './components/Preview.svelte'
   import presets from './presets'
   import makeStores from './stores'
 
   const debug = false
-  const sat = 40
+  const sat = 45
 
   const { boardStore, paletteStore, stateStore } = makeStores({
     board: { width: 12, height: 12 },
@@ -27,6 +28,11 @@
     <Palette {paletteStore} swatchSize="2rem" {sat} {stateStore} />
     <Eraser {stateStore} />
   </div>
+
+  <div class="previews">
+    <Preview {boardStore} {paletteStore} cellSize="0.5rem" {sat} {debug} />
+    <Preview {boardStore} {paletteStore} cellSize="0.25rem" {sat} {debug} />
+  </div>
 </div>
 
 <div>
@@ -45,5 +51,27 @@
     display: grid;
     gap: 1rem;
     height: fit-content;
+  }
+
+  .previews {
+    display: flex;
+    flex-direction: column;
+    border: 1px solid #00000022;
+    width: 8rem;
+    height: 16rem;
+    position: relative;
+    justify-content: space-evenly;
+    align-items: center;
+  }
+
+  .previews::before {
+    content: 'Preview';
+    position: absolute;
+    top: 0;
+    left: 0;
+    padding: 0.25rem;
+    background: #ffffffaa;
+    color: #00000066;
+    font-family: monospace;
   }
 </style>
---
+++ src/routes/grid2/components/Preview.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  import type { BoardStore, PaletteStore, StateStore } from '../stores'
+
+  import Cell from './DrawingBoard/BoardCell.svelte'
+
+  export let boardStore: BoardStore
+  export let paletteStore: PaletteStore
+  export let cellSize: string
+  export let debug = false
+  export let sat: number
+</script>
+
+<div>
+  {#each $boardStore as row}
+    <div class="row">
+      {#each row as cell}
+        <Cell {cell} {cellSize} {sat} {paletteStore} {debug} />
+      {/each}
+    </div>
+  {/each}
+</div>
+
+<style>
+  .row {
+    display: flex;
+  }
+</style>
