5bf6f741e9ecce3826b88b5c235f08ac7514f191
Daniel Reszka <dannn.r@gmail.com>
2023-06-14 17:02:37 +0200

    stores: extract paint(), simplify

--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -1,5 +1,6 @@
-import { get, writable, type Writable } from 'svelte/store'
+import { writable } from 'svelte/store'
 import adjust from './stores/adjust'
+import { paint } from './stores/paint'
 
 export type Board = { cols: number; values: string[] }
 export type Tool = 'brush' | 'eraser' | 'fill' | 'adjust'
@@ -16,63 +17,10 @@ export default function makeStores(cols: number, rows: number) {
     adjust: adjust(board),
     board,
     currentColor,
+    currentTool,
     isPainting,
     paint: (index: number) => paint(index, currentTool, currentColor, board),
-    setCurrentColor: (nextColor: string) => setCurrentColor(nextColor, currentColor),
-    setCurrentTool: (nextTool: Tool) => setCurrentTool(nextTool, currentTool),
-    currentTool,
+    setCurrentColor: (nextColor: string) => currentColor.set(nextColor),
+    setCurrentTool: (nextTool: Tool) => () => currentTool.set(nextTool),
   }
-}
-
-function setCurrentTool(nextTool: Tool, currentTool: Writable<Tool>) {
-  return () => currentTool.set(nextTool)
-}
-
-function setCurrentColor(nextColor: string, currentColor: Writable<string>) {
-  currentColor.set(nextColor)
-}
-
-function paint(
-  index: number,
-  currentTool: Writable<Tool>,
-  currentColor: Writable<string>,
-  board: Writable<Board>
-) {
-  const cols = get(board).cols
-  const rows = get(board).values.length / cols
-
-  if (get(currentTool) === 'eraser') {
-    board.update(($board) => {
-      $board.values[index] = 'none'
-      return $board
-    })
-    return
-  }
-
-  if (get(currentTool) === 'fill') {
-    const color = get(board).values[index]
-    const queue = [index]
-    const visited = new Set<number>()
-    while (queue.length > 0) {
-      const index = queue.pop()! // eslint-disable-line @typescript-eslint/no-non-null-assertion
-      visited.add(index)
-      board.update(($board) => {
-        $board.values[index] = get(currentColor)
-        return $board
-      })
-      const neighbors = [index - 1, index + 1, index - cols, index + cols]
-      for (const neighbor of neighbors) {
-        if (neighbor < 0 || neighbor >= cols * rows) continue
-        if (visited.has(neighbor)) continue
-        if (get(board).values[neighbor] !== color) continue
-        queue.push(neighbor)
-      }
-    }
-    return
-  }
-
-  board.update(($board) => {
-    $board.values[index] = get(currentColor)
-    return $board
-  })
 }
---
+++ src/lib/stores/paint.ts
@@ -0,0 +1,54 @@
+import { get, type Writable } from 'svelte/store'
+import type { Board, Tool } from '../stores'
+
+export function paint(
+  index: number,
+  currentTool: Writable<Tool>,
+  currentColor: Writable<string>,
+  board: Writable<Board>
+) {
+  const cols = get(board).cols
+  const rows = get(board).values.length / cols
+
+  if (get(currentTool) === 'brush') {
+    board.update(($board) => {
+      $board.values[index] = get(currentColor)
+      return $board
+    })
+  }
+
+  if (get(currentTool) === 'eraser') {
+    board.update(($board) => {
+      $board.values[index] = 'none'
+      return $board
+    })
+    return
+  }
+
+  if (get(currentTool) === 'fill') {
+    const color = get(board).values[index]
+    const queue = [index]
+    const visited = new Set<number>()
+    while (queue.length > 0) {
+      const index = queue.pop()! // eslint-disable-line @typescript-eslint/no-non-null-assertion
+      visited.add(index)
+      board.update(($board) => {
+        $board.values[index] = get(currentColor)
+        return $board
+      })
+      const neighbors = [index - 1, index + 1, index - cols, index + cols]
+      for (const neighbor of neighbors) {
+        if (neighbor < 0 || neighbor >= cols * rows) continue
+        if (visited.has(neighbor)) continue
+        if (get(board).values[neighbor] !== color) continue
+        queue.push(neighbor)
+      }
+    }
+    return
+  }
+
+  board.update(($board) => {
+    $board.values[index] = get(currentColor)
+    return $board
+  })
+}
