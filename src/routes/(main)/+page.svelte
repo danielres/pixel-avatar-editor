8f030102a9bc883d5952465287d52f4d63ad5c72
Daniel Reszka <dannn.r@gmail.com>
2023-06-16 16:25:18 +0200

    add tabs placeholders

--- src/routes/(main)/+page.svelte
+++ src/routes/(main)/+page.svelte
@@ -6,14 +6,24 @@
   import Ratio from '$lib/components/Ratio.svelte'
   import Tools from '$lib/components/Tools.svelte'
   import makeStores from '$lib/stores'
-
+  import { Info, Image, DownloadCloud, Brush } from 'lucide-svelte'
   const stores = makeStores(12, 12)
   const { board, currentTool, restorePreviousTool } = stores
 </script>
 
 <div class="shell">
+  <h1>
+    <img class="logo" src="https://www.pigg.gy/piggy.png" alt="logo" />
+  </h1>
+
   <header>
-    <div class="card">HEADER</div>
+    <img class="logo-name" src="https://www.pigg.gy/logo_02.png" alt="site name" />
+    <ul class="tabs">
+      <li class="active"><button><Brush /></button></li>
+      <li><button><Image /></button></li>
+      <li><button><DownloadCloud /></button></li>
+      <li><button><Info /></button></li>
+    </ul>
   </header>
 
   <aside>
@@ -44,14 +54,58 @@
   .shell {
     height: calc(100svh);
     display: grid;
-    grid-template-columns: auto 1fr;
+    grid-template-columns: 4.5rem 1fr;
     grid-template-rows: auto 1fr;
     gap: 1rem;
-    padding: 1rem;
+    padding-inline: 1rem;
+    padding-bottom: 1rem;
+  }
+
+  h1 {
+    display: flex;
+    align-items: end;
+    justify-items: center;
+    /* border: 1px solid red; */
+  }
+
+  .logo {
+    width: 4rem;
+    margin-inline: auto;
+    position: relative;
+    top: 0.5rem;
+    filter: drop-shadow(0 0 10px #fffbfe);
   }
 
   header {
-    grid-column: 1 / -1;
-    background: #ccc;
+    display: flex;
+  }
+
+  .logo-name {
+    height: 1.25rem;
+    align-self: end;
+    filter: drop-shadow(0 0 10px #fff4fd);
+    position: relative;
+    left: -1.25rem;
+    display: none;
+  }
+
+  header ul.tabs {
+    margin-left: 2rem;
+    display: flex;
+    align-self: end;
+    gap: 1rem;
+    position: relative;
+    top: calc(1rem + 2px);
+  }
+
+  header ul.tabs button {
+    padding: 0.6rem 0.75rem;
+    border-radius: 10% 10% 0 0;
+    color: hsl(0, 0%, 70%);
+  }
+
+  header ul.tabs li.active button {
+    background: white;
+    color: hsl(0, 0%, 0%);
   }
 </style>
