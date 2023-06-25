04ba00e5ac5a3cfe8b54588f947075ae2afde340
Daniel Reszka <dannn.r@gmail.com>
2023-06-25 14:21:38 +0200

    hooks: authjs: enable DrizzleAdapter

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,8 +1,11 @@
 import { GITHUB_ID, GITHUB_SECRET } from '$env/static/private'
+import { DrizzleAdapter } from '$lib/auth/adapter-drizzle'
 import GitHub from '@auth/core/providers/github'
 import { SvelteKitAuth } from '@auth/sveltekit'
+import { db } from './db/db'
 
 export const handle = SvelteKitAuth({
+  adapter: DrizzleAdapter(db),
   //@ts-expect-error https://github.com/nextauthjs/next-auth/issues/6174
   providers: [GitHub({ clientId: GITHUB_ID, clientSecret: GITHUB_SECRET })],
 })
