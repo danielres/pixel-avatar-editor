530a204e62f2bdbec80d9f5fc40ca367e7327a8f
Daniel Reszka <dannn.r@gmail.com>
2023-06-27 13:31:55 +0200

    $lib/components: add BoardPreview.svelte

---
+++ src/lib/components/BoardPreview.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import type { Board } from '$lib/usePigggy'
+
+  export let board: Board
+</script>
+
+<div class="board" style:grid-template-columns="repeat({board.cols}, 1fr)">
+  {#each board.values as value, i}
+    <div class="cell" style:background={value} />
+  {/each}
+</div>
+
+<style>
+  .board {
+    display: grid;
+  }
+
+  .cell {
+    aspect-ratio: 1;
+    cursor: crosshair;
+  }
+</style>
