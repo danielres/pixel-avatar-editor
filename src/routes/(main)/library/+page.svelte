dbc62ad2a11b5883596e40e480e64bbd046eb834
Daniel Reszka <dannn.r@gmail.com>
2023-06-29 09:23:46 +0200

    library: improve

--- src/routes/(main)/library/+page.svelte
+++ src/routes/(main)/library/+page.svelte
@@ -17,64 +17,89 @@
 </script>
 
 <div class="space-y-8">
-  <div class="flex gap-4">
-    {#each [...data.drawings].reverse() as drawing}
-      <div class="grid w-24 border border-black/50" class:ring={activeId === drawing.id}>
-        <button on:click={() => (activeId = drawing.id)}>
-          <Checkerboard>
-            <BoardPreview board={drawing.data} />
-          </Checkerboard>
-        </button>
-      </div>
-    {/each}
-  </div>
+  <section>
+    <h2>Your creations</h2>
+    <div class="flex gap-4">
+      {#each [...data.drawings].reverse() as drawing}
+        <div class="grid w-24 border border-black/50" class:ring={activeId === drawing.id}>
+          <button on:click={() => (activeId = drawing.id)}>
+            <Checkerboard>
+              <BoardPreview board={drawing.data} />
+            </Checkerboard>
+          </button>
+        </div>
+      {/each}
+    </div>
+  </section>
 
   {#if active}
-    <div class="flex gap-4">
-      <div class="grid w-full border border-black/50">
-        <Checkerboard>
-          <BoardPreview board={active.data} />
-        </Checkerboard>
-      </div>
+    <section>
+      <h2>Preview</h2>
+      <div class="flex gap-4 shadow-md">
+        <div class="grid w-full border border-black/50">
+          <div class="grid grid-cols-4 gap-4 p-4">
+            <!-- <div class="col-span-4">
+              <Checkerboard>
+                <BoardPreview board={active.data} />
+              </Checkerboard>
+            </div> -->
+            <div class="col-span-2">
+              <Checkerboard>
+                <BoardPreview board={active.data} />
+              </Checkerboard>
+            </div>
+            <div class="grid">
+              <div>
+                <Checkerboard>
+                  <BoardPreview board={active.data} />
+                </Checkerboard>
+              </div>
 
-      <div>
-        <ul>
-          <li>
-            <button
-              on:click={() => {
-                if (!active) return
-                board.snapshot()
-                board.set(active.data)
-                goto('/')
-              }}
-              class="flex p-2 gap-2 w-full"
-            >
-              <Brush />Edit
-            </button>
-          </li>
-          <li>
-            <form
-              action="?/delete"
-              method="post"
-              use:enhance={({ formData }) => {
-                if (!active) return
-                formData.append('id', active.id)
-                return async ({ update }) => {
-                  await update()
-                  activeId = data.drawings[data.drawings.length - 1].id
-                }
-              }}
-            >
-              <button type="submit" class="flex p-2 gap-2 w-full"><Trash2 />Delete</button>
-            </form>
-          </li>
-          <li>
-            <button class="flex p-2 gap-2 w-full whitespace-nowrap">
-              <DownloadCloud />Download PNG
-            </button>
-          </li>
-        </ul>
+              <ul>
+                <li>
+                  <button
+                    on:click={() => {
+                      if (!active) return
+                      board.snapshot()
+                      board.set(active.data)
+                      goto('/')
+                    }}
+                    class="flex p-2 gap-2 w-full"
+                  >
+                    <Brush />Edit
+                  </button>
+                </li>
+                <li>
+                  <form
+                    action="?/delete"
+                    method="post"
+                    use:enhance={({ formData }) => {
+                      if (!active) return
+                      formData.append('id', active.id)
+                      return async ({ update }) => {
+                        await update()
+                        activeId = data.drawings[data.drawings.length - 1].id
+                      }
+                    }}
+                  >
+                    <button type="submit" class="flex p-2 gap-2 w-full"><Trash2 />Delete</button>
+                  </form>
+                </li>
+                <li>
+                  <button class="flex p-2 gap-2 w-full whitespace-nowrap">
+                    <DownloadCloud />Download PNG
+                  </button>
+                </li>
+              </ul>
+            </div>
+            <div class="w-1/2">
+              <Checkerboard>
+                <BoardPreview board={active.data} />
+              </Checkerboard>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
+    </section>
   {/if}
 </div>
