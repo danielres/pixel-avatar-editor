ea0be59918e0daba885f10f4e22b7404d31d46a7
Daniel Reszka <dannn.r@gmail.com>
2023-06-29 10:10:12 +0200

    use md5 hash as drawing id + handle unique constraint errors

--- src/routes/(main)/+page.server.ts
+++ src/routes/(main)/+page.server.ts
@@ -1,16 +1,30 @@
 import type { Actions } from './$types'
 
 import { drawings } from '$db/schema'
-import { uuid } from '$lib/utils/uuid'
+import { fail } from '@sveltejs/kit'
+import md5 from 'blueimp-md5'
 
 export const actions: Actions = {
   async save({ request, locals: { user, db } }) {
     const jsonBoard = (await request.formData()).get('board') as string
     const data = JSON.parse(jsonBoard)
+    const hash = md5(jsonBoard)
 
-    db.insert(drawings)
-      .values({ data, authorId: user.id, id: uuid(), createdAt: new Date() })
-      .returning()
-      .get()
+    try {
+      db.insert(drawings)
+        .values({ data, authorId: user.id, id: hash, createdAt: new Date() })
+        .returning()
+        .get()
+    } catch (err) {
+      if (
+        err instanceof Error &&
+        err.name === 'SqliteError' &&
+        err.message.includes('UNIQUE constraint failed')
+      ) {
+        return fail(409, { _message: 'Drawing already exists' })
+      } else {
+        throw err
+      }
+    }
   },
 }
