ddb89260d37706cb36670633b089e4ba6210e555
Daniel Reszka <dannn.r@gmail.com>
2023-07-04 07:00:18 +0200

    /home: featured: improve db query using callback

--- src/routes/(home)/+page.server.ts
+++ src/routes/(home)/+page.server.ts
@@ -1,12 +1,9 @@
 import type { PageServerLoad } from './$types'
 
-import { drawings } from '$db/schema'
-import { desc } from 'drizzle-orm'
-
 export const load = (async ({ locals: { db } }) => {
   const featured = db.query.drawings.findMany({
     with: { author: { columns: { name: true, id: true } } },
-    orderBy: [desc(drawings.createdAt)],
+    orderBy: (drawings, { desc }) => [desc(drawings.createdAt)],
   })
 
   return {
