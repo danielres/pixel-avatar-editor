083a65151c64bed5626b357856fd313217189842
Daniel Reszka <dannn.r@gmail.com>
2023-07-04 14:34:49 +0200

    UI: setup popup

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,14 @@
 <script lang="ts">
-  import '@skeletonlabs/skeleton/themes/theme-rocket.css'
   import '@skeletonlabs/skeleton/styles/skeleton.css'
+  import '@skeletonlabs/skeleton/themes/theme-rocket.css'
   import '../app.postcss'
 
-  import { Modal } from '@skeletonlabs/skeleton'
+  import { arrow, autoUpdate, computePosition, flip, offset, shift } from '@floating-ui/dom'
+  import { Modal, storePopup } from '@skeletonlabs/skeleton'
+
+  storePopup.set({ computePosition, autoUpdate, offset, shift, flip, arrow })
 </script>
 
 <Modal background="bg-red-500" />
+
 <slot />
