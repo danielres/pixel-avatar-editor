a1c6fd5948c2a939eccf472b422efcdcf0150b5c
Daniel Reszka <dannn.r@gmail.com>
2023-07-06 10:06:09 +0200

    components: RangeBar: add

---
+++ src/lib/components/RangeBar.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import { ProgressBar } from '@skeletonlabs/skeleton'
+
+  export let value: number
+  export let min = 0
+  export let max = 100
+  export let step = 12.5
+</script>
+
+<div class="stack">
+  <input type="range" bind:value class="opacity-0 h-0" {min} {max} {step} />
+  <ProgressBar {value} max={100} />
+</div>
