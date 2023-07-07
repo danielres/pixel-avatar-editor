a789410f6744babbd94b9e4984d7c72659efa280
Daniel Reszka <dannn.r@gmail.com>
2023-07-07 09:22:51 +0200

    lib/utils/string: add REGEX_ONLY_LETTERS_NUMBERS_AND_SPACES

--- src/lib/utils/string.ts
+++ src/lib/utils/string.ts
@@ -1,3 +1,5 @@
 export function capitalizeFirstLetter(string: string) {
   return string.charAt(0).toUpperCase() + string.slice(1)
 }
+4
+export const REGEX_ONLY_LETTERS_NUMBERS_AND_SPACES = /^[a-zA-Z0-9 ]+$/
